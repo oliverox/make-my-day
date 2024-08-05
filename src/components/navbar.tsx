3bd7dc962c27e015e6a905b3f1ddd2543d2862b5
Oliver Oxenham <oliver.oxenham@gmail.com>
2024-08-05 22:45:37 +0400

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export function Navbar() {
   return (
     <div className="w-full py-2 text-right">
-      <Button variant="outline" size="icon">
+      <Button variant="ghost" size="icon" className="hover:bg-primary hover:text-primary-foreground">
         <MenuIcon className="h-5 w-5" />
       </Button>
     </div>
