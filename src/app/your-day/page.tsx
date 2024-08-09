a00688a0e5c80ebf09359fb7ee7d3d0185673af1
Oliver Oxenham <oliver.oxenham@gmail.com>
2024-08-09 15:41:00 +0400

    ai api call within page

--- src/app/your-day/page.tsx
+++ src/app/your-day/page.tsx
@@ -1,4 +1,3 @@
-import type { z } from "zod";
 import {
   Card,
   CardContent,
@@ -15,15 +14,23 @@ import {
   SparklesIcon,
   DollarSignIcon,
 } from "lucide-react";
-import type { AiJson } from "~/app/definitions/schemas";
+import { openai } from "@ai-sdk/openai";
+import { generateObject } from "ai";
+import { AiJson } from '~/app/definitions/schemas';
 import { UserComponentWrapper } from "~/components/userComponentWrapper";
+import type { z } from 'zod';
 
 export default async function YourDayPage() {
-  const resp = await fetch(`${process.env.BASE_URL}/api/ai`, {
-    method: "POST",
+  const model = openai.chat("gpt-4o");
+  const aiOutput = await generateObject({
+    model,
+    schema: AiJson,
+    mode: "json",
+    system: "You are a master itinerary planner specialized in Mauritius. You come up with creative, fun, unique and exciting activities based on the requirements of the user. You output the detailed itinerary in JSON format. You return only the JSON with no additional description or context.",
+    prompt: "I want a full day itinerary for a family of four, including two kids aged 4 and 8, to the north of Mauritius starting with breakfast at 9AM to the evening with dinner at 8PM. What makes this trip unique is that it should include local food delights and restaurant names and contact number, kid-friendly activities as well as ample time to rest, and finally unique spots to visits that are not so commonly known by the public. Keep the tone friendly and educational. Make sure the locations proposed exist and restaurants are in operation.",
   });
-  type AiJsonType = z.infer<typeof AiJson>;
-  const aiJson = (await resp.json()) as AiJsonType;
+  const aiJson = await aiOutput.toJsonResponse().json() as z.infer<typeof AiJson>;
+
   return (
     <UserComponentWrapper>
       <div className="flex flex-col gap-2">
