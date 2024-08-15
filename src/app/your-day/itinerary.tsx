c935c704d4442c665844b1999bdf17042d574e5c
Oliver Oxenham <oliver.oxenham@gmail.com>
2024-08-15 10:34:45 +0400

    test api route

--- src/app/your-day/itinerary.tsx
+++ src/app/your-day/itinerary.tsx
@@ -1,41 +1,43 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import {
-  PhoneIcon,
-  MapPinIcon,
-  UtensilsIcon,
-  SparklesIcon,
-  DollarSignIcon,
-} from "lucide-react";
-import { openai } from "@ai-sdk/openai";
-import { generateObject } from "ai";
-import { AiJson } from "~/app/definitions/schemas";
-import type { z } from "zod";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "~/components/ui/card";
+// import {
+//   PhoneIcon,
+//   MapPinIcon,
+//   UtensilsIcon,
+//   SparklesIcon,
+//   DollarSignIcon,
+// } from "lucide-react";
+// import { openai } from "@ai-sdk/openai";
+// import { generateObject } from "ai";
+// import { AiJson } from "~/app/definitions/schemas";
+// import type { z } from "zod";
 
 export async function Itinerary({ date }: { date: string }) {
-  const model = openai.chat("gpt-4o");
-  const aiOutput = await generateObject({
-    model,
-    schema: AiJson,
-    mode: "json",
-    system:
-      "You are a master itinerary planner specialized in Mauritius. You come up with creative, fun, unique and exciting activities based on the requirements of the user. You output the detailed itinerary in JSON format. You return only the JSON with no additional description or context.",
-    prompt: `I want a full day itinerary for a family of four, including two kids aged 4 and 8, to the north of Mauritius on ${date} starting with breakfast at 9AM to the evening with dinner at 8PM. What makes this trip unique is that it should include local food delights and restaurant names and contact number, kid-friendly activities as well as ample time to rest, and finally unique spots to visits that are not so commonly known by the public. Keep the tone friendly and educational. Make sure the locations proposed exist and restaurants are in operation.`,
-  });
+  // const model = openai.chat("gpt-4o");
+  // const aiOutput = await generateObject({
+  //   model,
+  //   schema: AiJson,
+  //   mode: "json",
+  //   system:
+  //     "You are a master itinerary planner specialized in Mauritius. You come up with creative, fun, unique and exciting activities based on the requirements of the user. You output the detailed itinerary in JSON format. You return only the JSON with no additional description or context.",
+  //   prompt: `I want a full day itinerary for a family of four, including two kids aged 4 and 8, to the north of Mauritius on ${date} starting with breakfast at 9AM to the evening with dinner at 8PM. What makes this trip unique is that it should include local food delights and restaurant names and contact number, kid-friendly activities as well as ample time to rest, and finally unique spots to visits that are not so commonly known by the public. Keep the tone friendly and educational. Make sure the locations proposed exist and restaurants are in operation.`,
+  // });
 
-  const aiJson = (await aiOutput.toJsonResponse().json()) as z.infer<
-    typeof AiJson
-  >;
+  // const aiJson = (await aiOutput.toJsonResponse().json()) as z.infer<
+  //   typeof AiJson
+  // >;
 
+  const aiJson = (await fetch(`/api/ai?date=${date}`)).json();
+  console.log('aiJson=', aiJson);
   return (
-    <div className="flex flex-col gap-4">
-      {aiJson.itinerary.map((item, index: number) => {
+    <div className="flex flex-col gap-4"><span>testing...</span>
+      {/* {aiJson.itinerary.map((item, index: number) => {
         return (
           <Card key={index}>
             <CardHeader>
@@ -84,7 +86,7 @@ export async function Itinerary({ date }: { date: string }) {
             <CardFooter className="flex flex-col items-start"></CardFooter>
           </Card>
         );
-      })}
+      })} */}
     </div>
   );
 }
